57751a7489238f52e0d1bc5bc69308456c0de517
ahsen.mermer <ahsen.mermer@n2mobil.com.tr>
2024-05-19 17:56:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {Intro} from './src/pages';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Intro);
